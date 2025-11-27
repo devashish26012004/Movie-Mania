38a3764a1aa32d91b4f180ebf61527985f1d164a
Devashish Dumbhare <devashishimp26@gmail.com>
2025-11-27 17:13:43 +0530

    UI changes in Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,29 +4,29 @@ const Navbar = () => {
   return (
     <>
       <div className="flex justify-center">
-        <div className="text-lg rounded-full border border-gray-400 w-2xl mt-5 py-3 px-4 box bg-[#e0e0e0] shadow-md hover:bg-gray-300 hover:translate-y-[-3px] transform transition duration-100">
-          <nav className="flex justify-evenly font-bold">
+        <div className="text-lg rounded-full border border-gray-400 w-2xl mt-5 py-3 px-4 box bg-[#e0e0e0] shadow-md hover:translate-y-[-3px] transform transition duration-100">
+          <nav className="flex justify-evenly font-semibold opacity-85">
             <Link
               to={"/"}
-              className="hover:scale-110 transition-transform duration-100"
+              className="hover:scale-110 transition-transform duration-100 hover:underline underline-offset-2"
             >
               Home
             </Link>
             <Link
               to={"/movies"}
-              className="hover:scale-110 transition-transform duration-100"
+              className="hover:scale-110 transition-transform duration-100 hover:underline underline-offset-2"
             >
               Movies
             </Link>
             <Link
               to={"/dashboard"}
-              className="hover:scale-110 transition-transform duration-100"
+              className="hover:scale-110 transition-transform duration-100 hover:underline underline-offset-2"
             >
               Dashboard
             </Link>
             <Link
               to={"/Profile"}
-              className="hover:scale-110 transition-transform duration-100"
+              className="hover:scale-110 transition-transform duration-100 hover:underline underline-offset-2"
             >
               Profile
             </Link>
